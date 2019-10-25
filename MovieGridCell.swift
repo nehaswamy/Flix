817d074a76ac69bc202630f91803d324b6312190
Neha Swamy <nehaswamy10@gmail.com>
2019-10-25 15:49:23 -0400

    Part 2

---
+++ MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Neha Swamy on 10/20/19.
+//  Copyright © 2019 Neha Swamy. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+}
