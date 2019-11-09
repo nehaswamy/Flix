842aad71d0c55a5b07b784ef46732f03916649c4
nehaswamy <44928687+nehaswamy@users.noreply.github.com>
2019-11-09 14:40:10 -0500

    Update MovieDetailsViewController.swift

--- MovieDetailsViewController.swift
+++ MovieDetailsViewController.swift
@@ -7,24 +7,47 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MovieDetailsViewController: UIViewController {
-
+    var movie: [String: Any]!
+    @IBOutlet var backdrop: UIImageView!
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var synopsis: UITextView!
+    @IBOutlet var ratingLabel: UILabel!
+    @IBOutlet var releaseDateLabel: UILabel!
+    
+    @IBOutlet var moviePoster: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        
+        synopsis.text = movie["overview"] as? String
+        synopsis.sizeToFit()
+        synopsis.textContainer.lineBreakMode = NSLineBreakMode.byWordWrapping
+        synopsis.isScrollEnabled = true
+        synopsis.isUserInteractionEnabled = true
+        synopsis.showsVerticalScrollIndicator = true
+       
+        let posterBaseUrl = "http://image.tmdb.org/t/p/w500"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: posterBaseUrl + posterPath)
+        moviePoster.af_setImage(withURL: posterUrl!)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "http://image.tmdb.org/t/p/w780" + backdropPath)
+        backdrop.af_setImage(withURL: backdropUrl!)
+        
+        let rating:String = String(format: "%@", movie["vote_average"] as! CVarArg)
+        
+        ratingLabel.text = "Rating: " + rating
+        
+        let releaseDate:String = String(format: "%@", movie["release_date"] as! CVarArg)
+        
+        releaseDateLabel.text = "Release date: " + releaseDate
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+ 
+
